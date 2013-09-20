5ad7a33fd77e42ca83d715cc5549c7e2d480fa8a
Drew Shefman <dshefman@squaredi.com>
2013-09-20 15:45:36 -0500

    updating to correct jquery

--- public/uhtwitter.js
+++ public/uhtwitter.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
     console.log("ready");
     $("#send").click(function() {
-      console.log("onClick")
+      console.log("onClick");
       var req = jQuery.post("/postTweet", {
-          'firstName': $('firstName').val(),
-          'lastName' : $('lastName').val()},
+          'firstName': $('#firstName').val(),
+          'lastName' : $('#lastName').val()},
           'json').error(function(){
               alert("an error occured");
 
           }).success(function(data){
-              console.log("redirect to " +data)
+              console.log("redirect to " +data);
               window.location = data;
           })
       })
-    })
+    });
